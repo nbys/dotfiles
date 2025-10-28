bf62501fcf692139571b940e545c8d4458dce6d3
Nikolay Bystritskiy <nikolay.bystritskiy@gmail.com>
2025-10-28 10:56:38 +0100

    changes in lsp config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -3,235 +3,214 @@ local diagnostic_signs = require("util.icons").diagnostic_signs
 local typescript_organise_imports = require("util.lsp").typescript_organise_imports
 
 local config = function()
-	require("neoconf").setup({})
-	local cmp_nvim_lsp = require("cmp_nvim_lsp")
-	local lspconfig = require("lspconfig")
-	local capabilities = cmp_nvim_lsp.default_capabilities()
+  require("neoconf").setup({})
+  local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-	for type, icon in pairs(diagnostic_signs) do
-		local hl = "DiagnosticSign" .. type
-		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-	end
+  for type, icon in pairs(diagnostic_signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  end
 
-	-- lua
-	lspconfig.lua_ls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		settings = { -- custom settings for lua
-			Lua = {
-				-- make the language server recognize "vim" global
-				diagnostics = {
-					globals = { "vim" },
-				},
-				workspace = {
-					-- make language server aware of runtime files
-					library = {
-						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-						[vim.fn.stdpath("config") .. "/lua"] = true,
-					},
-				},
-			},
-		},
-	})
+  -- helper for roots (new API)
+  local function root_with(markers)
+    return function(fname)
+      return vim.fs.root(fname, markers)
+    end
+  end
 
-	-- python
-	lspconfig.pyright.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		settings = {
-			pyright = {
-				disableOrganizeImports = false,
-				analysis = {
-					useLibraryCodeForTypes = true,
-					autoSearchPaths = true,
-					diagnosticMode = "workspace",
-					autoImportCompletions = true,
-				},
-			},
-		},
-	})
+  -- Lua
+  vim.lsp.config("lua_ls", {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+      Lua = {
+        diagnostics = { globals = { "vim" } },
+        workspace = {
+          library = {
+            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+            [vim.fn.stdpath("config") .. "/lua"] = true,
+          },
+          checkThirdParty = false,
+        },
+      },
+    },
+  })
 
-	lspconfig.gopls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		settings = {
-			pyright = {
-				disableOrganizeImports = false,
-				analysis = {
-					useLibraryCodeForTypes = true,
-					autoSearchPaths = true,
-					diagnosticMode = "workspace",
-					autoImportCompletions = true,
-				},
-			},
-		},
-	})
+  -- Python
+  vim.lsp.config("pyright", {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+      pyright = { disableOrganizeImports = false },
+      python = {
+        analysis = {
+          useLibraryCodeForTypes = true,
+          autoSearchPaths = true,
+          diagnosticMode = "workspace",
+          autoImportCompletions = true,
+        },
+      },
+    },
+  })
 
-	lspconfig.golangci_lint_ls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		default_config = {
-			cmd = { "golangci-lint-langserver" },
-			root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
-			init_options = {
-				command = {
-					"golangci-lint",
-					"run",
-					"--enable-all",
-					"--disable",
-					"lll",
-					"--out-format",
-					"json",
-					"--issues-exit-code=1",
-				},
-			},
-		},
-	})
+  -- Go
+  vim.lsp.config("gopls", {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    root_dir = root_with({ "go.mod", ".git" }),
+    settings = {
+      gopls = {
+        gofumpt = true,
+        staticcheck = true,
+        analyses = { unusedparams = true, shadow = true },
+        hints = { parameterNames = true, rangeVariableTypes = true },
+      },
+    },
+  })
 
-	lspconfig.rust_analyzer.setup({
-		capabilities = capabilities,
-		---on_attach = on_attach,
-		on_attach = function(client, bufnr)
-			-- Call your custom on_attach function
-			on_attach(client, bufnr)
+  -- golangci-lint-langserver
+  vim.lsp.config("golangci_lint_ls", {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    cmd = { "golangci-lint-langserver" },
+    root_dir = root_with({ "go.mod", ".git" }),
+    init_options = {
+      command = {
+        "golangci-lint",
+        "run",
+        "--enable-all",
+        "--disable", "lll",
+        "--out-format", "json",
+        "--issues-exit-code=1",
+      },
+    },
+  })
 
-			-- Enable format on save if supported
-			if client.server_capabilities.documentFormattingProvider then
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.format({ timeout_ms = 100 })
-					end,
-				})
-			end
-		end,
-		filetypes = { "rust" },
-		root_dir = lspconfig.util.root_pattern("Cargo.toml"),
-		settings = {
-			["rust-analyzer"] = {
-				cargo = {
-					allFeatures = true,
-				},
-			},
-		},
-	})
-	-- typescript
-	lspconfig.ts_ls.setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-		filetypes = {
-			"typescript",
-			"javascript",
-			"typescriptreact",
-			"javascriptreact",
-		},
-		commands = {
-			TypeScriptOrganizeImports = typescript_organise_imports,
-		},
-		settings = {
-			typescript = {
-				indentStyle = "space",
-				indentSize = 2,
-			},
-		},
-		root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
-	})
+  -- Rust
+  vim.lsp.config("rust_analyzer", {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+      on_attach(client, bufnr)
+      if client.server_capabilities.documentFormattingProvider then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ timeout_ms = 100 })
+          end,
+        })
+      end
+    end,
+    root_dir = root_with({ "Cargo.toml", ".git" }),
+    settings = { ["rust-analyzer"] = { cargo = { allFeatures = true } } },
+  })
 
-	-- SAP CDS
-	lspconfig.cds_lsp.setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-	})
+  -- TypeScript / JavaScript
+  vim.lsp.config("ts_ls", {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+    commands = { TypeScriptOrganizeImports = typescript_organise_imports },
+    settings = { typescript = { indentStyle = "space", indentSize = 2 } },
+    root_dir = root_with({ "package.json", "tsconfig.json", ".git" }),
+  })
 
-	-- docker
-	lspconfig.dockerls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-	})
-	--
-	-- C/C++
-	lspconfig.clangd.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		cmd = {
-			"clangd",
-			"--offset-encoding=utf-16",
-		},
-	})
+  -- SAP CDS (adjust server name if yours differs)
+  vim.lsp.config("cds_lsp", {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
 
-	-- Erlang
-	lspconfig.erlangls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		filetypes = { "erlang" },
-		-- root_dir = util.root_pattern("rebar.config", "erlang.mk", ".git"),
-		-- single_file_support = true,
-	})
+  -- Docker
+  vim.lsp.config("dockerls", {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    filetypes = { "dockerfile" },
+  })
 
-	--
-	local luacheck = require("efmls-configs.linters.luacheck")
-	local stylua = require("efmls-configs.formatters.stylua")
-	local gofmt = require("efmls-configs.formatters.gofmt")
-	local goimports = require("efmls-configs.formatters.goimports")
-	-- local golangci_lint = require("efmls-configs.linters.golangci_lint")
-	local eslint = require("efmls-configs.linters.eslint")
-	local prettier = require("efmls-configs.formatters.prettier")
-	local cpplint = require("efmls-configs.linters.cpplint")
-	local clangformat = require("efmls-configs.formatters.clang_format")
+  -- C/C++
+  vim.lsp.config("clangd", {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    cmd = { "clangd", "--offset-encoding=utf-16" },
+  })
 
-	local ruff_linter = require("efmls-configs.linters.ruff")
-	local ruff_formatter = require("efmls-configs.formatters.ruff")
-    local mypy = require('efmls-configs.linters.mypy')
+  -- Erlang
+  vim.lsp.config("erlangls", {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    filetypes = { "erlang" },
+  })
 
-	-- configure efm server
-	lspconfig.efm.setup({
-		filetypes = {
-			"lua",
-			"python",
-			"go",
-			"rust",
-			"javascript",
-			"typescript",
-			"vue",
-			"docker",
-			"c",
-			"cpp",
-		},
-		init_options = {
-			documentFormatting = true,
-			documentRangeFormatting = true,
-			hover = true,
-			documentSymbol = true,
-			codeAction = true,
-			completion = true,
-		},
-		settings = {
-			languages = {
-				lua = { luacheck, stylua },
-				python = { ruff_linter, ruff_formatter, mypy },
-				go = { gofmt, goimports },
-				typescript = { eslint, prettier },
-				javascript = { eslint, prettier },
-				typescriptreact = { eslint, prettier },
-				vue = { eslint, prettier },
-				docker = { prettier },
-				c = { clangformat, cpplint },
-				cpp = { clangformat, cpplint },
-			},
-		},
-	})
+  -- EFM aggregator
+  local luacheck      = require("efmls-configs.linters.luacheck")
+  local stylua        = require("efmls-configs.formatters.stylua")
+  local gofmt         = require("efmls-configs.formatters.gofmt")
+  local goimports     = require("efmls-configs.formatters.goimports")
+  local eslint        = require("efmls-configs.linters.eslint")
+  local prettier      = require("efmls-configs.formatters.prettier")
+  local cpplint       = require("efmls-configs.linters.cpplint")
+  local clangformat   = require("efmls-configs.formatters.clang_format")
+  local ruff_linter   = require("efmls-configs.linters.ruff")
+  local ruff_formatter= require("efmls-configs.formatters.ruff")
+  local mypy          = require("efmls-configs.linters.mypy")
+
+  vim.lsp.config("efm", {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    filetypes = {
+      "lua","python","go","rust","javascript","typescript","typescriptreact",
+      "vue","dockerfile","c","cpp",
+    },
+    init_options = {
+      documentFormatting = true,
+      documentRangeFormatting = true,
+      hover = true,
+      documentSymbol = true,
+      codeAction = true,
+      completion = true,
+    },
+    settings = {
+      languages = {
+        lua = { luacheck, stylua },
+        python = { ruff_linter, ruff_formatter, mypy },
+        go = { gofmt, goimports },
+        typescript = { eslint, prettier },
+        javascript = { eslint, prettier },
+        typescriptreact = { eslint, prettier },
+        vue = { eslint, prettier },
+        dockerfile = { prettier },
+        c = { clangformat, cpplint },
+        cpp = { clangformat, cpplint },
+      },
+    },
+  })
+
+  -- finally enable all at once
+  vim.lsp.enable({
+    "lua_ls",
+    "pyright",
+    "gopls",
+    "golangci_lint_ls",
+    "rust_analyzer",
+    "ts_ls",
+    "cds_lsp",
+    "dockerls",
+    "clangd",
+    "erlangls",
+    "efm",
+  })
 end
 
 return {
-	"neovim/nvim-lspconfig",
-	config = config,
-	lazy = false,
-	dependencies = {
-		"windwp/nvim-autopairs",
-		"williamboman/mason.nvim",
-		"creativenull/efmls-configs-nvim",
-		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-nvim-lsp",
-	},
+  "neovim/nvim-lspconfig",
+  config = config,
+  lazy = false,
+  dependencies = {
+    "windwp/nvim-autopairs",
+    "williamboman/mason.nvim",
+    "creativenull/efmls-configs-nvim",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-nvim-lsp",
+  },
 }
