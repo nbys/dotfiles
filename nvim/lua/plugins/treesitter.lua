823d95770fa21950eea66232005d37862fc57ed6
Nikolay Bystritskiy <nikolay.bystritskiy@gmail.com>
2026-04-02 14:58:02 +0200

    treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,66 +1,64 @@
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.cds = {
-	install_info = {
-		url = "https://github.com/cap-js-community/tree-sitter-cds.git",
-		branch = "main",
-		files = { "src/parser.c", "src/scanner.c" },
-	},
-	filetype = "cds",
-}
 local config = function()
-	require("nvim-treesitter.configs").setup({
-		build = ":TSUpdate",
-		indent = {
-			enable = true,
-		},
-		autotag = {
-			enable = true,
-		},
-		event = {
-			"BufReadPre",
-			"BufNewFile",
-		},
-		ensure_installed = {
-			"rust",
-			"markdown",
-			"json",
-			"javascript",
-			"typescript",
-			"yaml",
-			"html",
-			"css",
-			"markdown",
-			"bash",
-			"lua",
-			"dockerfile",
-			"solidity",
-			"gitignore",
-			"python",
-			"vue",
-			"svelte",
-			"toml",
-			"cds",
-			"erlang",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = true,
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<C-s>",
-				node_incremental = "<C-s>",
-				scope_incremental = false,
-				node_decremental = "<BS>",
-			},
-		},
-	})
+  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+  parser_config.cds = {
+    install_info = {
+      url = "https://github.com/cap-js-community/tree-sitter-cds.git",
+      branch = "main",
+      files = { "src/parser.c", "src/scanner.c" },
+    },
+    filetype = "cds",
+  }
+
+  require("nvim-treesitter.configs").setup({
+    indent = {
+      enable = true,
+    },
+    autotag = {
+      enable = true,
+    },
+    ensure_installed = {
+      "rust",
+      "markdown",
+      "json",
+      "javascript",
+      "typescript",
+      "yaml",
+      "html",
+      "css",
+      "bash",
+      "lua",
+      "dockerfile",
+      "solidity",
+      "gitignore",
+      "python",
+      "vue",
+      "svelte",
+      "toml",
+      "cds",
+      "erlang",
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-s>",
+        node_incremental = "<C-s>",
+        scope_incremental = false,
+        node_decremental = "<BS>",
+      },
+    },
+  })
 end
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	config = config,
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  build = ":TSUpdate",
+  lazy = false,
+  config = config,
 }
